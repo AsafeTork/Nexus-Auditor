8494c3e9de1b3d42fb5d6f799efa2b0f6ba3eec7
* <asafetork@gmail.com>
2026-04-30 16:43:37 -0300

    fix: shorten alembic revision ids to fit version_num

--- migrations/versions/0009_add_verification_loop.py
+++ migrations/versions/0009_add_verification_loop.py
@@ -1,7 +1,7 @@
 """add verification loop
 
 Revision ID: 0009_add_verification_loop
-Revises: 0008_add_monitoring_decision_json
+Revises: 0008_monitoring_decision
 Create Date: 2026-04-29
 """
 
@@ -11,7 +11,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = "0009_add_verification_loop"
-down_revision = "0008_add_monitoring_decision_json"
+down_revision = "0008_monitoring_decision"
 branch_labels = None
 depends_on = None
 
@@ -57,4 +57,3 @@ def downgrade() -> None:
         op.drop_column("monitoring_runs", "verification_json")
     except Exception:
         pass
-
